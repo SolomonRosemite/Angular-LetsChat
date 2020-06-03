71e426e76374101f6d0f57e6624a6be3dd353803
SolomonRosemite™ <solomonrosemiteprojects@gmail.com>
2020-06-03 19:49:07 +0200

    Added Eventemitter for Later...

---
+++ letschat/src/app/event-emitter.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventEmitterService {
+  invokeFirstComponentFunction = new EventEmitter();
+  subsVar: Subscription;
+
+  constructor() {}
+
+  onFirstComponentButtonClick() {
+    this.invokeFirstComponentFunction.emit();
+  }
+}
