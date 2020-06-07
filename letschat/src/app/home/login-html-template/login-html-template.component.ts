197c0d3928c8da5a78dc6691b8e3314f94a75585
SolomonRosemite™ <solomonrosemiteprojects@gmail.com>
2020-06-08 01:10:54 +0200

    Added Google Login to Sigh in Page

--- letschat/src/app/home/login-html-template/login-html-template.component.ts
+++ letschat/src/app/home/login-html-template/login-html-template.component.ts
@@ -1,13 +1,21 @@
 import { EventEmitterService } from 'src/app/event-emitter.service';
 import { Component, OnInit } from '@angular/core';
 
+import { BackendServiceService } from './../../services/backend/backend-service.service';
+
+import { AuthService } from '../../services/auth/auth.service';
+
 @Component({
   selector: 'app-login-html-template',
   templateUrl: './login-html-template.component.html',
   styleUrls: ['./../AuthPage/authpage.component.scss'],
 })
 export class LoginHtmlTemplateComponent implements OnInit {
-  constructor(private eventEmitterService: EventEmitterService) {}
+  constructor(
+    private eventEmitterService: EventEmitterService,
+    public auth: AuthService,
+    private backend: BackendServiceService
+  ) {}
 
   validData = false;
   buttonColor = '#27AE60';
