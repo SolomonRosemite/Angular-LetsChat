ae4d424a12fdcce224aac5a1502a1bfb05c3427f
SolomonRosemite™ <solomonrosemiteprojects@gmail.com>
2020-06-24 01:36:44 +0200

    Few UI Improvements

--- letschat/src/app/chat/common/file-reference-card/file-reference-card.component.ts
+++ letschat/src/app/chat/common/file-reference-card/file-reference-card.component.ts
@@ -1,5 +1,6 @@
 import { FileReference } from './../../../services/Models/FileReference.model';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-file-reference-card',
@@ -7,17 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-reference-card.component.scss'],
 })
 export class FileReferenceCardComponent implements OnInit {
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
 
   fileReference: FileReference;
+  imageUrl = '../../../../assets/images/file-Icon.png';
 
   ngOnInit(): void {
     this.fileReference = new FileReference({
       date: '20.02.2020',
-      fileFileReferenceUrl: '',
+      fileFileReferenceUrl:
+        'https://static01.nyt.com/images/2019/11/05/science/28TB-SUNSET1/merlin_163473282_fe17fc6b-78b6-4cdd-b301-6f63e6ebdd7a-superJumbo.jpg',
       filename: 'SchoolHomework From Ms.Skrrr',
       fullFilename: 'SchoolHomework From Ms.Skrrr.pdf',
     });
+
+    switch (this.fileReference.getFileType().toLowerCase()) {
+      case 'png':
+        this.imageUrl = this.fileReference.fileFileReferenceUrl;
+        break;
+      case 'gif':
+        this.imageUrl = this.fileReference.fileFileReferenceUrl;
+        break;
+      case 'jpg':
+        this.imageUrl = this.fileReference.fileFileReferenceUrl;
+        break;
+      case 'jpeg':
+        this.imageUrl = this.fileReference.fileFileReferenceUrl;
+        break;
+    }
   }
 
   public displayFilename(filename: string): string {
@@ -25,5 +43,9 @@ export class FileReferenceCardComponent implements OnInit {
       return filename.substring(0, 15) + '...';
     }
     return filename;
+  }
+
+  public downloadFile(): void {
+    // TODO: Add Download function.
   }
 }
