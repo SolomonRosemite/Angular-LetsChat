9444e867ef29ad3baaca1e84c717298f041544f5
SolomonRosemite™ <solomonrosemiteprojects@gmail.com>
2020-06-25 15:47:32 +0200

    Small changes to the messages page

--- letschat/src/app/chat/chat-page/chat-messages-page/chat-messages-page.component.ts
+++ letschat/src/app/chat/chat-page/chat-messages-page/chat-messages-page.component.ts
@@ -6,7 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-messages-page.component.scss'],
 })
 export class ChatMessagesPageComponent implements OnInit {
+  messages: Message[];
   constructor() {}
 
-  ngOnInit(): void {}
+  message: string;
+
+  ngOnInit(): void {
+    this.messages = [
+      {
+        message: 'Hey, do you have to do homework for Math?',
+        who: 'receiver',
+      },
+      {
+        message: 'Sure.',
+        who: 'me',
+      },
+      {
+        message: 'I just send them. Check the Shared Files.',
+        who: 'me',
+      },
+      {
+        message: 'Thanks!😀',
+        who: 'receiver',
+      },
+      {
+        // tslint:disable-next-line: quotemark
+        message: "You're Welcome.",
+        who: 'me',
+      },
+    ];
+  }
+
+  public sendMessage(): void {
+    console.log(this.message);
+  }
+}
+
+interface Message {
+  message: string;
+  who: string;
 }
