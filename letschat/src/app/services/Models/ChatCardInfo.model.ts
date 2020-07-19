9cb457c0bf3257f677975a1c75ad915366b1eb56
Solomon Rosemite™ <60587271+SolomonRosemite@users.noreply.github.com>
2020-07-19 20:59:58 +0200

    Create ChatCardInfo.model.ts

---
+++ letschat/src/app/services/Models/ChatCardInfo.model.ts
@@ -0,0 +1,27 @@
+export class ChatCardInfo {
+  constructor({
+    displayName,
+    chatId,
+    senderUid,
+    receiverUid,
+    photoURL,
+    latestMessage,
+    date: lastSeen,
+  }: ChatCardInfo) {
+    this.displayName = displayName;
+    this.chatId = chatId;
+    this.senderUid = senderUid;
+    this.receiverUid = receiverUid;
+    this.photoURL = photoURL;
+    this.date = lastSeen;
+    this.latestMessage = latestMessage;
+  }
+
+  readonly displayName: string;
+  readonly chatId: string;
+  readonly photoURL: string;
+  readonly receiverUid: string;
+  readonly senderUid: string;
+  readonly date: string;
+  latestMessage: string;
+}
